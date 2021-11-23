b80ae5bbb32d389846eb192331497d166895a359
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-11-23 18:00:07 -0300

    requisito 12 concluido

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const taskListId = '#lista-tarefas';
+
 function clearInput() {
   document.querySelector('#texto-tarefa').value = '';
 }
@@ -20,7 +22,7 @@ function riskItem(event) {
 
 function createTask() {
   const textInput = document.querySelector('#texto-tarefa').value;
-  const taskList = document.querySelector('#lista-tarefas');
+  const taskList = document.querySelector(taskListId);
   const newli = document.createElement('li');
   newli.innerText = textInput;
   newli.addEventListener('click', changeBgColor);
@@ -30,7 +32,7 @@ function createTask() {
 }
 
 function clearTasks() {
-  const taskList = document.querySelector('#lista-tarefas');
+  const taskList = document.querySelector(taskListId);
   taskList.innerHTML = '';
 }
 
@@ -41,6 +43,11 @@ function removeCompleted() {
   });
 }
 
+function saveTaskList() {
+  const taskList = document.querySelector(taskListId).innerHTML;
+  localStorage.setItem('data', taskList);
+}
+
 const newTaskBtn = document.querySelector('#criar-tarefa');
 newTaskBtn.addEventListener('click', createTask);
 
@@ -49,3 +56,14 @@ clearBtn.addEventListener('click', clearTasks);
 
 const removeCompletedBtn = document.querySelector('#remover-finalizados');
 removeCompletedBtn.addEventListener('click', removeCompleted);
+
+const saveTaskBtn = document.querySelector('#salvar-tarefas');
+saveTaskBtn.addEventListener('click', saveTaskList);
+
+// https://gomakethings.com/saving-html-to-localstorage-with-vanilla-js/#:~:text=Save%20the%20HTML%20to%20localStorage%20%23&text=The%20innerHTML%20property%20returns%20the,to%20save%20data%20to%20localStorage%20.
+function loadTaskList() {
+  const data = localStorage.getItem('data');
+  const taskList = document.querySelector(taskListId);
+  taskList.innerHTML = data;
+}
+loadTaskList();
