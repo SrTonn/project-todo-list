532a03c5a0d1576a67aaf87008b30fbf20cd5018
Wellington Rodrigues <ton.rodrigues@icloud.com>
2021-11-23 14:51:15 -0300

    requisito 07 concluido

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ function clearInput() {
   document.querySelector('#texto-tarefa').value = '';
 }
 
+function changeBgColor(event) {
+  const element = event.target;
+  element.style.backgroundColor = 'rgb(128,128,128)';
+}
+
 function createTask() {
   const textInput = document.querySelector('#texto-tarefa').value;
   const taskList = document.querySelector('#lista-tarefas');
   const newli = document.createElement('li');
   newli.innerText = textInput;
+  newli.addEventListener('click', changeBgColor);
   taskList.appendChild(newli);
   clearInput();
 }
